2f3dd118d376eb3be53bdcd7ecadc01da12ef60c
kedabudre <abdurehimk77@gmail.com>
2024-12-07 01:15:38 +0300

    update hackerroom rotaion effect

--- src/components/HeroCamera.jsx
+++ src/components/HeroCamera.jsx
@@ -9,8 +9,13 @@ const HeroCamera = ({ children, isMobile }) => {
     easing.damp3(groupRef.current.position, [0, 0, 8], 0.25, delta);
 
     if (!isMobile) {
-      easing.dampE(groupRef.current.rotation, [-state.pointer.y / 3, state.pointer.x / 5, 0])
+      // Clamp the x-rotation to be between -Math.PI/2 and Math.PI/2 (half a circle)
+      const clampedX = Math.max(-Math.PI / 2, Math.min(Math.PI / 2, -state.pointer.y / 3));
+
+      // Apply the dampE function with the clamped value for rotation
+      easing.dampE(groupRef.current.rotation, [clampedX, state.pointer.x / 5, 0]);
     }
+
   })
 
   return (
--- src/constants/index.js
+++ src/constants/index.js
@@ -250,10 +250,10 @@ export const calculateSizes = (isSmall, isMobile, isTablet) => {
   return {
     deskScale: isSmall ? 0.05 : isMobile ? 0.06 : 0.065,
     deskPosition: isMobile ? [0.5, -4.5, 0] : [0.25, -5.5, 0],
-    cubePosition: isSmall ? [5.8, -8, 0] : isMobile ? [8, -5, 0] : isTablet ? [11, -5, 0] : [15, -6.5, 0],
-    reactLogoPosition: isSmall ? [5, 3, 0] : isMobile ? [8, 4, 0] : isTablet ? [10, 4, 0] : [20, 3, 0],
-    ringPosition: isSmall ? [-10, 8, 0] : isMobile ? [-14, 10, 0] : isTablet ? [-12, 10, 0] : [-34, 10, 5],
-    targetPosition: isSmall ? [-7, -16, -10] : isMobile ? [-9, -13, -10] : isTablet ? [-11, -12, -10] : [-15, -13, -10],
+    cubePosition: isSmall ? [4, -5, 0] : isMobile ? [5, -5, 0] : isTablet ? [5, -5, 0] : [12, -6, 0],
+    reactLogoPosition: isSmall ? [3, 4, 0] : isMobile ? [5, 4, 0] : isTablet ? [5, 4, 0] : [14, 3, 0],
+    ringPosition: isSmall ? [-5, 7, 0] : isMobile ? [-10, 10, 0] : isTablet ? [-12, 10, 0] : [-35, 6, 0],
+    targetPosition: isSmall ? [-5, -10, -10] : isMobile ? [-9, -10, -10] : isTablet ? [-11, -7, -10] : [-18, -13, -10],
   };
 };
 
